0031af17f1c476ebb4e5093798111a1d38dd73cb
flankero9745 <flankero2146@gmail.com>
2024-10-14 14:53:20 +0300

    add: initial files

--- src/theme.js
+++
@@ -1,6 +0,0 @@
-const theme = {
-  others: { FONT_SIZE_171B2C: "15px", FONT_SIZE_171EED: "16px", LINE_HEIGHT_172A30: "19px", FONT_SIZE_569E1233: "15.5px" },
-  colors: { COLOR_9700F7DA: "#414042", COLOR_9C758683: "#777777" },
-};
-
-export { theme };
